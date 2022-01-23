d3451486e698032e3b1d1ec336ce40074db32df5
Shreni Singh <shrenisingh0307@gmail.com>
2022-01-23 18:25:57 +0530

    todo app

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/page/home_page.dart';
+import 'package:todo/provider/todo_provider.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Todo App With Firebase';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => TodosProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(
+            primarySwatch: Colors.pink,
+            scaffoldBackgroundColor: Color(0xFFf6f5ee),
+          ),
+          home: HomePage(),
+        ),
+      );
+}
